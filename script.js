69a74e0474b03c1a92a6d937d2066ca443db42c3
orangecat <shaunchua.edu@gmail.com>
2023-03-25 22:34:41 +0800

    add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
 function addition(x, y) {
     return x + y;
 }
@@ -14,4 +18,19 @@ function division(x, y) {
     return x / y;
 }
 
+function operate(x, y, operator) {
 
+    switch (operator) {
+        case '+':
+            return addition(x, y);
+
+        case '-':
+            return subtraction(x, y);
+        
+        case '*':
+            return subtraction(x, y);
+        
+        case '/':
+            return subtraction(x, y);
+    }
+}
